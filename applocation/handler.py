f5ac8681f4905b6368228e924a883c840699e0b1
Nodirbek <ccnodirbekcc@gmail.com>
2024-10-16 20:32:08 +0500

    fixed bugs

--- applocation/handler.py
+++ applocation/handler.py
@@ -3,7 +3,7 @@ from aiogram.filters import Command,CommandStart
 from aiogram.types import Message
 
 router = Router()
-@router.message(CommandStart)
+@router.message(CommandStart())
 async def cmd_start(message:Message):
     await message.answer("assalomu aleykum bratim!")
 
